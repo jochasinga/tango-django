e98db1ec17894b08486abb696afdfadd5e1f30db
Nokjao <jo.chasinga@gmail.com>
2014-04-25 10:38:48 +0700

    Add some more stuff to forms.py and add_category.html

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -1,22 +1,28 @@
+# This form.py is in fact a model exclusively dedicated to Django forms
+
 from django import forms
 from rango.models import Page, Category
 
+# Class inherites from django.forms.ModelForm
 class CategoryForm(forms.ModelForm):
+    "Form class presenting data Category Model"
     name = forms.CharField(max_length=128, help_text="Please enter the category name.")
     visits = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     # An inline class to provide additional information on the form
     class Meta:
-        # Provide an association between the ModelForm and a model
+        "Provide an association between the ModelForm and the Category Model"
         model = Category
         
 class PageForm(forms.ModelForm):
+    "Form class presenting data from Page Model"
     title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
-    urls = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     
     def clean(self):
+        "Method to clean data coming into the forms"
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
 
@@ -24,7 +30,8 @@ class PageForm(forms.ModelForm):
         if url and not url.startswith('http://'):
             url = 'http://' + url
             cleaned_data['url'] = url
-
+        # Return a dictionary cleaned_data = {'url': url}
+        # Always return this dict as cleaned_data to avoid error
         return cleaned_data
 
     class Meta:
